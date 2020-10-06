a6a0060b069678e9e7e6ffcf63a8ca28ee51cbb1
Tanmay Dhuri <63371774+tanmayd1996@users.noreply.github.com>
2020-10-05 18:22:29 -0700

    updated directory

--- Blog_Supplements/database-inmemory-on-SPARC/ssb_load/scripts/mv-inmemory.sql
+++
@@ -1,5 +0,0 @@
-alter table ssb.part inmemory memcompress for query high priority critical;
-alter table ssb.supplier inmemory memcompress for query high priority critical;
-alter table ssb.date_dim inmemory memcompress for query high priority critical;
-alter table ssb.customer inmemory memcompress for query high priority critical;
-alter table ssb.lineorder inmemory memcompress for query high priority critical;
